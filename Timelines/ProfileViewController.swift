c32657489f4cedd67a94fc7e74301a201d44c2a0
Thang Tong <tongthang0104@gmail.com>
2015-11-03 15:36:32 -0700

    add profileViewController

--- Timelines/ProfileViewController.swift
+++ Timelines/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Timelines
 //
 //  Created by Thang H Tong on 11/3/15.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
