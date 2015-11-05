49c11572ef22e2a1c94b3e2bee23678d3f06b5be
Thang Tong <tongthang0104@gmail.com>
2015-11-04 18:05:15 -0700

    ImageController added

---
+++ Timelines/ImageController.swift
@@ -0,0 +1,21 @@
+//
+//  ImageController.swift
+//  Timelines
+//
+//  Created by Thang H Tong on 11/4/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageController {
+    
+    static func uploadIamge(image: UIImage, completion: (identifier: String) -> Void) {
+        completion(identifier: "-K1l4125TYvKMc7rcp5e")
+    }
+    
+    static func imageForIdentifier(identifier: String, completion: (image: UIImage?) -> Void) {
+        completion(image: UIImage(named: "Hamburger"))
+    }
+}
