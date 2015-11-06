f50f1ef71d81b5641b06ecdd0381711c2c0c779d
Thang Tong <tongthang0104@gmail.com>
2015-11-06 00:33:36 -0700

    UserSearchResultViewController created

---
+++ Timelines/UserSearchResultsTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  UserSearchResultsTableViewController.swift
+//  Timelines
+//
+//  Created by Thang H Tong on 11/5/15.
+//  Copyright © 2015 Thang. All rights reserved.
+//
+
+import UIKit
+
+class UserSearchResultsTableViewController: UITableViewController {
+   
+    var usersResultsDataSource: [User] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+ 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usersResultsDataSource.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("userNameCell", forIndexPath: indexPath)
+        let users = usersResultsDataSource[indexPath.row]
+        cell.textLabel?.text = users.username
+        return cell
+    }
+    
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        self.presentingViewController?.performSegueWithIdentifier("toProfileView", sender: cell)
+    }
+
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
