3b01353ec24489e754b1d6b5d9ab973acf21ad70
Thang Tong <tongthang0104@gmail.com>
2015-11-07 23:32:04 -0700

    UserSearchTableViewController updated
    cleaned up and add segue

--- Timelines/UserSearchTableViewController.swift
+++ Timelines/UserSearchTableViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class UserSearchTableViewController: UITableViewController, UISearchResultsUpdating {
-
+    
     //MARK: Properties
     
     var userDataSource: [User] = []
@@ -28,9 +28,9 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
                 
             case .Friends:
                 
-        //TODO: -Why it has to be changed to followers ? and why use the followByUser but not follow User
-                UserController.followByUser(UserController.shareController.currentUser, completion: { (followers) -> Void in
-                    completion(users: followers)
+                //TODO: -Why it has to be changed to followers ? and why use the followByUser but not follow User
+                UserController.followByUser(UserController.shareController.currentUser, completion: { (followed) -> Void in
+                    completion(users: followed)
                 })
             case .All:
                 UserController.fetchAllUsers({ (users) -> Void in
@@ -47,7 +47,7 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
         }
     }
     
-   
+    
     func updateViewBasedOnMode() {
         
         mode.users { (users) -> Void in
@@ -59,15 +59,16 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
         }
         self.tableView.reloadData()
     }
-   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         updateViewBasedOnMode()
         setUpSearchController()
+        
     }
     
-
+    
     //MARK: Changing Value Segment Action
     
     @IBAction func selectedIndexChanged(sender: UISegmentedControl) {
@@ -80,22 +81,22 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return userDataSource.count
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("usersCell", forIndexPath: indexPath)
-
+        
         let users = userDataSource[indexPath.row]
         
         cell.textLabel?.text =  users.username
-
+        
         return cell
     }
     
@@ -129,50 +130,38 @@ class UserSearchTableViewController: UITableViewController, UISearchResultsUpdat
             
         }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
+    }*/
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toProfileView" {
+            
+            guard let cell = sender as? UITableViewCell else {return}
+            
+            if let profileViewDisplay = segue.destinationViewController as? ProfileViewController {
+                
+                if let indexPath = tableView.indexPathForCell(cell) {
+                    
+                    let users = userDataSource[indexPath.row]
+                    
+                    profileViewDisplay.users = users
+                } else if let indexPath = (searchController.searchResultsController as? UserSearchResultsTableViewController)?.tableView.indexPathForCell(cell) {
+                    if let searchResult = (searchController.searchResultsController as? UserSearchResultsTableViewController) {
+                        let users = searchResult.usersResultsDataSource[indexPath.row]
+                        profileViewDisplay.users = users
+                    }
+                }
+            }
+        }
     }
-    */
-
 }
